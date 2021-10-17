c4f167d71e52cdce041101b4ea31d1a33de3e98e
Michał Brzozowski <58233942+brzozio@users.noreply.github.com>
2021-10-17 17:22:04 +0200

    Delete Pokoj.java

--- Pokoj.java
+++
@@ -1,9 +0,0 @@
-public class Pokoj {
-	public static void main(String[] args){
-
-		Lampa lampa = new Lampa(); // instancja Lampy w klasie Pokoj
-			lampa.obliczMoc();
-			System.out.println("Sumowalna moc zarowek w lampie to: " + (lampa.mocOgolna) + "W");
-			lampa.wlaczLosowaZarowke();
-	}	
-}
