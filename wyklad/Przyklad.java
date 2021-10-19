610dff35cdfc1c418a50baced6e24f9e7895bbe9
brzozio <58233942+brzozio@users.noreply.github.com>
2021-10-19 17:26:46 +0200

    another example

--- wyklad/Przyklad.java
+++ wyklad/Przyklad.java
@@ -1,18 +1,5 @@
 public class Przyklad {
-    public static void main(String[] args){
-        int tab[][] = new int[10][];
-        tab[0] = new int[2];
-        tab[2] = new int[5];
-
-        int ileWierszy = tab.length;
-        int suma = 0;
-        
-        for(int i = 0; i < ileWierszy; i++){
-            if(tab[i] != null){
-               for(int j = 0; j < tab[i].length; j++){
-                    suma += tab[i][j];
-                } 
-            }
-        }
+    public static void main(String[] args) {
+        System.out.println("xxx");
     }
 }
