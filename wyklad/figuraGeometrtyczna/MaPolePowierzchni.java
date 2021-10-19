ecff82bf1f306ec0577b427d437afaf0ea7be58e
brzozio <58233942+brzozio@users.noreply.github.com>
2021-10-19 18:30:03 +0200

    added default method info

--- wyklad/figuraGeometrtyczna/MaPolePowierzchni.java
+++ wyklad/figuraGeometrtyczna/MaPolePowierzchni.java
@@ -1,3 +1,11 @@
 public interface MaPolePowierzchni {
     public double polePowierzchni();
+
+
+// defaultowa implementacja wraz z implementacja interfejsu MaPolePowierzchni
+// nie tzeba ich dodawac do klas do ktorych interfejs jest implementowany w odroznieniu
+// od metod nie "default" ktore trzeba dodwac
+    default public double polePowierzchniInne(){  
+        return 0;
+    }
 }
