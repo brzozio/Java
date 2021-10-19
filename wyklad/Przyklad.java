b7738310b262f1ac6afce8642f57444e248f75bc
brzozio <58233942+brzozio@users.noreply.github.com>
2021-10-19 17:16:28 +0200

    minor changes

--- wyklad/Przyklad.java
+++ wyklad/Przyklad.java
@@ -3,5 +3,16 @@ public class Przyklad {
         int tab[][] = new int[10][];
         tab[0] = new int[2];
         tab[2] = new int[5];
+
+        int ileWierszy = tab.length;
+        int suma = 0;
+        
+        for(int i = 0; i < ileWierszy; i++){
+            if(tab[i] != null){
+               for(int j = 0; j < tab[i].length; j++){
+                    suma += tab[i][j];
+                } 
+            }
+        }
     }
 }
