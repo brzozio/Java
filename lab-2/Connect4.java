5a5e348ebbdadbcc2d19be38ac9ede429d0dd543
brzozio <58233942+brzozio@users.noreply.github.com>
2021-10-23 23:14:26 +0200

    array fixed

--- lab-2/Connect4.java
+++ lab-2/Connect4.java
@@ -2,10 +2,28 @@
 import java.util.Arrays;
 
 public class Connect4{
-	Tablice tablica = (Tablice) new Tablice(); // konstruktor bezparametrowy
+	//Tablice tablica[][] = new Tablice[iloscWierszyPrzekazywana][iloscKolumnPrzekazywana]; // konstruktor bezparametrowy
+	public Tablice tablica = new Tablice();
+	public Tablice tablica2[][] = new Tablice[6][7]; // konstruktor z dwoma argumentami
+			public void zerujTablice(){
+				//System.out.println(tablica.planszaGry[1][1]);
+				for(int i = 0; i < 6; i++){
+					for(int j = 0; j < 7; j++){
+						tablica.planszaGry[i][j] = 0;
+					}
+				}
+			}
 
-	Tablice tablica2[][] = new Tablice[6][7]; // konstruktor z dwoma argumentami
+	public void wyswietlajTablice(){
+		//System.out.println(tablica.planszaGry[1][1]);
+		for(int i = 0; i < 6; i++){
+			for(int j = 0; j < 7; j++){
+				System.out.println(tablica.planszaGry[i][j]);
+			}
+		}
+	}
 	
+	//Tablice tablica3 = new Tablice(6,7);
 	/* ----- ZASADY GRY --------
 	 * 0 -pusta komorka
 	 * 1 - krazek gracza Gracz1
@@ -21,27 +39,16 @@ public class Connect4{
 			} else return 1;
 		}
 	
-	//public boolean wrzucKrazek(int kolumna) {
-		//tablica[0][kolumna] == 0; // dodawanie krazka danego gracza
-		//czyjaKolej(); // zmiana kolejki na innego gracza
-		//if(czyPelnaKolumna(tablica, kolumna) == true) {
+	public boolean wrzucKrazek(int kolumna) {
+		if(tablica.planszaGry[0][kolumna] == 0){
 			
-		//}
+		} // dodawanie krazka danego gracza
+		czyjaKolej(); // zmiana kolejki na innego gracza
+		if(czyPelnaKolumna(tablica, kolumna) == true) {
+			
+		}
 		
 		
-	//}
-	public void wyswietlajTablice(){
-		for(int o = 0; o < 6; o++){
-		for(int j = 0; j < 7; j++){
-			//tablica[i][j] = 0;
-		}
 	}
-	for(int i = 0; i < 6; i++){
-		for(int j = 0; j < 7; j++){
-			System.out.println();
-		}
-	}
-	}
-	
 	
 }
