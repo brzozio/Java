cebf621b3dfc0c4289723d1e195fc2bfe65571c9
brzozio <58233942+brzozio@users.noreply.github.com>
2022-01-11 16:20:03 +0100

    small changes

--- lab-4/SudokuField.java
+++ lab-4/SudokuField.java
@@ -2,6 +2,17 @@ import javax.swing.JLabel;
 
 public class SudokuField extends JLabel{
     private int value;
+
+    public SudokuField(int value){
+        this.setSudokuField();
+    }
+
     protected void getSudokuField();
     protected void setSudokuField();
+
+    protected void setDisplayedValue(){
+        if(this.value != 0){
+
+        }
+    }
 }
