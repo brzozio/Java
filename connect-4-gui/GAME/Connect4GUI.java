16cb8bde2d9761043ec96b74c83a0dc785b20b82
brzozio <58233942+brzozio@users.noreply.github.com>
2022-01-24 20:00:52 +0100

    lot changes

---
+++ connect-4-gui/GAME/Connect4GUI.java
@@ -0,0 +1,34 @@
+public class Connect4GUI extends JFrame implements ColumnClickListener, GameStateListener {
+    //pola
+    int column,row,numberOfPlayers;
+    public Connect4GUI(int rows, int cols, int numberOfPlayers){
+        Connect4 gra = new Connect4();
+		if(rows == 0 || cols == 0){
+			gra = new Connect4(); // twrorzenie planszy 6x7
+		} else{
+			gra = new Connect4(rows,cols);
+			gra = new Connect4(); // tworzenie i tak planszy 6x7
+		} 
+		// Zerowanie tablicy przed gra
+		gra.zerujTablice();
+        this.createGUI();
+        GameStateListener = Connect4GUI.this;
+    }
+
+    public void createGUI(){
+
+    }
+
+    @Override
+    public void columnClicked(int column){
+
+    }
+    @Override
+    public void gameFinished(int winner){
+
+    }
+    @Override
+    public void waitingForPlayer(){
+
+    }
+}
